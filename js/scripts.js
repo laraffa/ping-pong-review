434deef4e25fe0a77fd817f86f5771a1359d36d3
lindsey <lindsey.raffa@gmail.com>
2016-01-22 09:48:04 -0800

    pass test to changed spec number two regarding pong output

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 var pingPong = function(inputNumber) {
   if (inputNumber % 3 === 0){
     return [1,2,'ping'];
+  } else if (inputNumber % 5 === 0) {
+    return [1,2,3,4,'pong'];
   }
 };
-  // } else if (inputNumber % 15 === 0) {
-//     return 'pong';
+
 //   } else if (inputNumber % 5 === 0) {
 //     return 'pingpong';
 //   }
--- spec/specs.js
+++ spec/specs.js
@@ -3,7 +3,7 @@ it("will replace numbers divisible by 3 with 'ping'", function(){
   expect(pingPong(3)).to.eql([1,2,'ping']);
   });
   it("replaces numbers divisible by 5 with with 'pong'", function(){
-    expect(pingPong(5)).to.eql(1,2,3,4,'pong');
+    expect(pingPong(5)).to.eql([1,2,3,4,'pong']);
   });
 // it("replaces numbers divisible by 5 with with 'pong'", function(){
 //   expect(pingPong(5)).to.equal('pong');
