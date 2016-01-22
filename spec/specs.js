8c1e9f9254dfa76d34b9d6b34338bafd758adde8
lindsey <lindsey.raffa@gmail.com>
2016-01-22 09:24:23 -0800

    add third spec which tests for ping pong

--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,7 @@ it("replaces numbers divisible by 3 with 'ping'", function() {
 it("replaces numbers divisible by 5 with with 'pong'", function(){
   expect(pingPong(5)).to.equal('pong');
   });
+it("replaces numbers divisible by 15 with with 'pingpong'", function(){
+  expect(pingPong(15)).to.equal('pingpong');
+  });
 });
