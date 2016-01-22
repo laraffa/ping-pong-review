37d59318beff7171da470cc7ccba52bfe9a5fb54
lindsey <lindsey.raffa@gmail.com>
2016-01-22 09:44:58 -0800

    adds new spec that makes output of numbers divisible by 5 as an array instead of a string

--- spec/specs.js
+++ spec/specs.js
@@ -1,12 +1,11 @@
 describe("pingPong", function(){
-it("replaces numbers divisible by 3 with 'ping'", function() {
-  expect(pingPong(3)).to.equal('ping');
+it("will replace numbers divisible by 3 with 'ping'", function(){
+  expect(pingPong(3)).to.eql([1,2,'ping']);
   });
-
-it("replaces numbers divisible by 5 with with 'pong'", function(){
-  expect(pingPong(5)).to.equal('pong');
+  it("replaces numbers divisible by 5 with with 'pong'", function(){
+    expect(pingPong(5)).to.eql(1,2,3,4,'pong');
   });
-it("replaces numbers divisible by 15 with with 'pingpong'", function(){
-  expect(pingPong(15)).to.equal('pingpong');
-  });
+// it("replaces numbers divisible by 5 with with 'pong'", function(){
+//   expect(pingPong(5)).to.equal('pong');
+//   });
 });
