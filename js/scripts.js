0fe9dfcea5b8fbbb27d6d04b9df460c29d1d37d7
lindsey <lindsey.raffa@gmail.com>
2016-01-22 10:56:46 -0800

    will fix mistakes in specs, and add a for loop to my code

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
 var pingPong = function(inputNumber) {
-  if (inputNumber % 3 === 0){
-    return [1,2,'ping'];
-  } else if (inputNumber % 5 === 0) {
-    return [1,2,3,4,'pong'];
-  } else if (inputNumber % 15 === 0) {
-    return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,'pingpong'];
-  }
+var numberSet = [];
 
+for (var i = 1; i <= inputNumber; i++) {
+  if (i % 15 === 0) {
+    numberSet.push("pingpong");
+  } else if (i % 3 === 0) {
+    numberSet.push("ping");
+  } else if (i % 5 === 0) {
+    numberSet.push("pong");
+  } else {
+    numberSet.push(i);
+  }
+}
+return numberSet;
 };
+
+//   if (inputNumber % 3 === 0){
+//     return [1,2,'ping'];
+//   } else if (inputNumber % 5 === 0) {
+//     return [1,2,3,4,'pong'];
+//   } else if (inputNumber % 15 === 0) {
+//     return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,'pingpong'];
+//   }
+//
+// };
--- spec/specs.js
+++ spec/specs.js
@@ -3,9 +3,9 @@ it("will replace numbers divisible by 3 with 'ping'", function(){
   expect(pingPong(3)).to.eql([1,2,'ping']);
   });
   it("replaces numbers divisible by 5 with with 'pong'", function(){
-    expect(pingPong(5)).to.eql([1,2,3,4,'pong']);
+    expect(pingPong(5)).to.eql([1,2,'ping',4,'pong']);
   });
-it("replaces numbers divisible by 15 with with 'pong'", function(){
-  expect(pingPong(15)).to.eql([1,2,3,4,5,6,7,8,9,10,11,12,13,14,'pingpong']);
+it("replaces numbers divisible by 15 with with 'pingpong'", function(){
+  expect(pingPong(15)).to.eql([1,2,'ping',4,'pong','ping',7,8,'ping','pong',11,'ping',13,14,'pingpong']);
   });
 });
