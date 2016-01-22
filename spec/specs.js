c67ad7a94801635d6f3f562d0ff9d157dc7d597c
lindsey <lindsey.raffa@gmail.com>
2016-01-22 09:22:20 -0800

    second spec 'pong' passed

--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,9 @@
 describe("pingPong", function(){
 it("replaces numbers divisible by 3 with 'ping'", function() {
   expect(pingPong(3)).to.equal('ping');
-});
+  });
 
 it("replaces numbers divisible by 5 with with 'pong'", function(){
   expect(pingPong(5)).to.equal('pong');
-});
+  });
 });
